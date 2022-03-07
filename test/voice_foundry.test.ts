7ca24c8c88629c6a880817da1bb8c6080d53f18a
Asen Tolsuzov <asen.tolsuzov@scalefocus.com>
2022-03-07 16:03:42 +0200

    init CDK

---
+++ test/voice_foundry.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VoiceFoundry from '../lib/voice_foundry-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/voice_foundry-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VoiceFoundry.VoiceFoundryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
